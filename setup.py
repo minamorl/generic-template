24ce4a7d0659ebfe45e8a9985d1ca122d382f84e
minamorl <minamorl@users.noreply.github.com>
2016-04-25 13:15:01 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "generictemplate",
-    version = "0.0.1",
-    packages = find_packages(),
-    install_requires = [
+    author="minamorl",
+    author_email="minamorl@minamorl.com"
+    name="generictemplate",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
         "jinja2"
     ]
 )
