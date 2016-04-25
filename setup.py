ce90da79171e87f03b55a8c3fb4c3aab0f3406e8
minamorl <minamorl@users.noreply.github.com>
2016-04-25 13:31:34 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     author="minamorl",
-    author_email="minamorl@minamorl.com"
+    author_email="minamorl@minamorl.com",
     name="generictemplate",
     version="0.0.1",
     packages=find_packages(),
