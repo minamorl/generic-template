8160a3296973cc8e6d289c36b38e65f6a20ab965
minamorl <minamorl@users.noreply.github.com>
2016-04-25 13:32:52 +0900

    Add url to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     author="minamorl",
     author_email="minamorl@minamorl.com",
     name="generictemplate",
+    url="https://github.com/minamorl/generic-template",
     version="0.0.1",
     packages=find_packages(),
     install_requires=[
